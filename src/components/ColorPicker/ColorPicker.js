e264e81d60c96c3924cf287c009b6423ba098c1b
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-06-07 16:10:28 +0300

    color picker animation

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,20 +1,49 @@
+import { Component } from 'react';
 import s from './ColorPicker.module.css';
 
-function ColorPicker({ options }) {
-  return (
-    <div className={s.container}>
-      <h2 className={s.title}>Color Picker</h2>
-      <div>
-        {options.map(option => (
-          <span
-            key={option.label}
-            className={s.option}
-            style={{ backgroundColor: option.color }}
-          ></span>
-        ))}
+class ColorPicker extends Component {
+  state = {
+    activeOptionIndex: 0,
+  };
+
+  setActiveIndex = index => {
+    this.setState({ activeOptionIndex: index });
+  };
+
+  makeOptionClassName = index => {
+    const optionClasses = [s.option];
+    if (index === this.state.activeOptionIndex) {
+      optionClasses.push(s.optionActive);
+    }
+
+    return optionClasses.join(' ');
+  };
+
+  render() {
+    const { activeOptionIndex } = this.state;
+    const { options } = this.props;
+
+    const { label } = options[activeOptionIndex];
+
+    return (
+      <div className={s.container}>
+        <h2 className={s.title}>Color Picker</h2>
+        <p>Выбран цвет: {label}</p>
+        <div>
+          {options.map(({ label, color }, index) => {
+            return (
+              <button
+                key={label}
+                className={this.makeOptionClassName(index)}
+                style={{ backgroundColor: color }}
+                onClick={() => this.setActiveIndex(index)}
+              ></button>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ColorPicker;
